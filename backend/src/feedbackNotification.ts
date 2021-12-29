9fec3732860e760db2a91344c4401f099a4f501e
Swizec Teller <swizec@swizec.com>
2021-12-29 15:19:46 -0800

    Cleanup code and limit notifications

--- backend/src/feedbackNotification.ts
+++ backend/src/feedbackNotification.ts
@@ -8,32 +8,65 @@ import {
 } from "@aws-sdk/client-secrets-manager";
 
 type Vote = {
-    createdAt: Date | null;
-    voteType?: "thumbsup" | "thumbsdown";
-    widgetId?: string;
-    answers: string | null;
-    instanceOfJoy?: string;
-    voteId?: string;
-    voter?: string;
+    createdAt: string;
+    voteType: "thumbsup" | "thumbsdown";
+    widgetId: string;
+    answers?: { [key: string]: string };
+    instanceOfJoy: string;
+    voteId: string;
+    voter: string;
 };
 
-function shouldNotify(record: DynamoDBRecord): boolean {
+type VoteRecord = DynamoDBRecord & {
+    vote: Vote;
+};
+
+function parseRecord(record: DynamoDBRecord): VoteRecord {
+    if (!record.dynamodb?.NewImage) {
+        throw new Error("Invalid DynamoDBRecord");
+    }
+
+    const vote = unmarshall(
+        record.dynamodb?.NewImage as {
+            [key: string]: AttributeValue;
+        }
+    );
+
+    if (typeof vote.answers === "string") {
+        vote.answers = JSON.parse(vote.answers);
+    }
+
+    return {
+        ...record,
+        vote: vote as Vote,
+    };
+}
+
+// decides if we should do the thing
+function shouldNotify(record: VoteRecord): boolean {
     // we only care about inserts and updates
     if (!(record.eventName === "INSERT" || record.eventName === "MODIFY")) {
         return false;
     }
 
-    // can't do much unless the new values are there
-    if (!record.dynamodb?.NewImage) {
+    // only send for Swizec's widgetIds
+    if (
+        ![
+            "1b23e2b6-1c2a-49a2-b3ee-69bb26c125e9",
+            "50c48f9e-66bd-47d0-9c78-4865719ad305",
+            "aab01040-bb89-40d9-8a2e-92ede0f8d82b",
+            "c02dba9a-2eed-48d8-acd8-5b1797b5b14a",
+            "e7e9ae80-73ac-45e7-84e0-38fc6b5443b1",
+            "e9819383-7166-4d69-a091-557bd7f73f22",
+            // test
+            "8e4d5e4a-7a51-4ecf-a577-c0a2255281d7",
+        ].includes(record.vote.widgetId)
+    ) {
         return false;
     }
 
     return true;
 }
-
-// function buildMessage(recordContents: { [key: string]: ) {
-//     // if (record
-// }
 
 // reads slack webhook url from secrets manager
 async function getSlackUrl() {
@@ -79,24 +112,18 @@ async function sendNotification(vote: Vote): Promise<void> {
 }
 
 export async function handler(event: DynamoDBStreamEvent) {
-    const votes = new Map();
+    const votes = new Map<string, Vote>();
 
     // collect latest instance of a vote
     // event processing happens in-order
     // https://docs.aws.amazon.com/lambda/latest/dg/with-ddb.html
     for (const record of event.Records) {
-        if (shouldNotify(record)) {
-            // shouldNotify guarantees NewImage is defined
-            const vote = unmarshall(
-                record.dynamodb!.NewImage! as {
-                    [key: string]: AttributeValue;
-                }
-            );
-            if (typeof vote.answers === "string") {
-                vote.answers = JSON.parse(vote.answers);
-            }
+        const voteRecord = parseRecord(record);
 
-            votes.set(vote.voteId, vote);
+        if (shouldNotify(voteRecord)) {
+            // shouldNotify guarantees NewImage is defined
+
+            votes.set(voteRecord.vote.voteId, voteRecord.vote);
         }
     }
 
