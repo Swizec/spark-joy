50e9be7c64e6e20d8acff8e2c93ec3adb59cd492
Swizec Teller <swizec@swizec.com>
2021-12-28 18:16:00 -0800

    Get almost clean vote to send

--- backend/src/feedbackNotification.ts
+++ backend/src/feedbackNotification.ts
@@ -1,5 +1,15 @@
 import { DynamoDBRecord, DynamoDBStreamEvent } from "aws-lambda";
 
+type Vote = {
+    createdAt: Date | null;
+    voteType?: string;
+    widgetId?: string;
+    answers: string | null;
+    instanceOfJoy?: string;
+    voteId?: string;
+    voter?: string;
+};
+
 function shouldNotify(record: DynamoDBRecord): boolean {
     // we only care about inserts and updates
     if (!(record.eventName === "INSERT" || record.eventName === "MODIFY")) {
@@ -18,17 +28,47 @@ function shouldNotify(record: DynamoDBRecord): boolean {
 //     // if (record
 // }
 
-async function sendNotification(record: DynamoDBRecord): Promise<void> {
+async function sendNotification(vote: Vote): Promise<void> {
+    console.log("Gonna send notification for", vote);
     // const message = buildMessage(record);
 }
 
+// transforms an event record to a vote
+function recordToVote(record: DynamoDBRecord): Vote {
+    if (!record.dynamodb?.NewImage) {
+        throw new Error("Invalid DB record");
+    }
+
+    // this feels like I'm missing a util library
+    return {
+        createdAt: record.dynamodb.NewImage.createdAt.S
+            ? new Date(record.dynamodb?.NewImage?.createdAt.S)
+            : null,
+        voteType: record.dynamodb.NewImage.voteType.S,
+        widgetId: record.dynamodb.NewImage.widgetId.S,
+        answers: record.dynamodb.NewImage.answers.S
+            ? JSON.parse(record.dynamodb.NewImage.answers.S)
+            : null,
+        instanceOfJoy: record.dynamodb.NewImage.instanceOfJoy.S,
+        voteId: record.dynamodb.NewImage.voteId.S,
+        voter: record.dynamodb.NewImage.voter.S,
+    };
+}
+
 export async function handler(event: DynamoDBStreamEvent) {
-    console.log("Handling stuff from dynamodb");
+    const votes = new Map();
+
+    // collect latest instance of a vote
+    // event processing happens in-order
+    // https://docs.aws.amazon.com/lambda/latest/dg/with-ddb.html
     for (const record of event.Records) {
-        console.log(record, record.dynamodb);
         if (shouldNotify(record)) {
-            console.log(record, record.dynamodb);
-            await sendNotification(record);
+            const vote = recordToVote(record);
+            votes.set(vote.voteId, vote);
         }
+    }
+
+    for (const [voteId, vote] of votes) {
+        await sendNotification(vote);
     }
 }
