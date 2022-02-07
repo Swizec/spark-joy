c46c9841d27d3cf77704e68fff4d218f31608a7e
Swizec Teller <swizec@swizec.com>
2022-02-06 16:46:25 -0800

    Render form based on data from graphql

--- remix-sparkjoy/app/routes/$widgetId/$voteType.tsx
+++ remix-sparkjoy/app/routes/$widgetId/$voteType.tsx
@@ -1,15 +1,84 @@
 import { LoaderFunction, useLoaderData } from "remix";
+import { gql, GraphQLClient } from "graphql-request";
 
-type LoaderData = { widgetId: string };
+type LoaderData = {
+    followupQuestions: FollowupQuestion[];
+};
+
+type Widget = {
+    userId: string;
+    widgetId: string;
+    widgetType: string;
+    followupQuestions: string;
+};
+
+type FollowupQuestion = {
+    label: string;
+    id: number;
+    type: "text" | "boolean";
+};
+
+const client = new GraphQLClient(
+    "https://56v8170tv6.execute-api.us-east-1.amazonaws.com/graphql"
+);
+
+// TODO: add support for fetching individual widgetId on server
+async function fetchFollowupQuestions(
+    widgetId: string
+): Promise<FollowupQuestion[]> {
+    const query = gql`
+        query {
+            allWidget {
+                userId
+                widgetId
+                widgetType
+                followupQuestions
+            }
+        }
+    `;
+
+    const data: { allWidget: Widget[] } = await client.request(query);
+
+    const widget = data.allWidget.find(
+        (widget) => widget.widgetId === widgetId
+    );
+
+    if (widget) {
+        return JSON.parse(widget.followupQuestions);
+    }
+
+    return [];
+}
 
 export const loader: LoaderFunction = async ({ params }) => {
-    console.log(params);
+    if (!params.widgetId) {
+        return null;
+    }
 
-    return params;
+    const followupQuestions = await fetchFollowupQuestions(params.widgetId);
+
+    return {
+        followupQuestions,
+    };
+};
+
+const Question = (props: FollowupQuestion) => {
+    return (
+        <div>
+            <label>{props.label}</label>
+            <input type="text"></input>
+        </div>
+    );
 };
 
 export default function FeedbackRoute() {
     const data = useLoaderData<LoaderData>();
 
-    return <div>{data.widgetId}</div>;
+    return (
+        <form>
+            {data.followupQuestions.map((q) => (
+                <Question {...q} />
+            ))}
+        </form>
+    );
 }
